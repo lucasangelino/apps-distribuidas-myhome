442ef397b0582310bb97fa2d389cda490ff12e93
Lucas Angelino <angelino.n.lucas@gmail.com>
2023-08-30 17:09:59 -0300

    add splash screen

--- App.tsx
+++ App.tsx
@@ -5,16 +5,24 @@
  * @format
  */
 
-import React from 'react';
+import React, {useEffect} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
 // components
 import Splash from './app/screens/Splash';
+import {Platform} from 'react-native';
+import SplashScreen from 'react-native-splash-screen';
 
 const Stack = createNativeStackNavigator();
 
 function App() {
+  useEffect(() => {
+    if (Platform.OS === 'android') {
+      SplashScreen.hide();
+    }
+  }, []);
+
   return (
     <NavigationContainer>
       <Stack.Navigator>
