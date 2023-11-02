f6ff424eff44c46e1aab79460ea1aef3a2b3ff94
lucasangelino <angelino.n.lucas@gmail.com>
2023-11-02 19:34:16 -0300

    add inmobiliaria profile

--- app/screens/inmobiliaria/InmobiliariaProfile.js
+++ app/screens/inmobiliaria/InmobiliariaProfile.js
@@ -1,8 +1,40 @@
 import React from 'react';
-import {Text} from 'react-native-paper';
+import { StyleSheet } from 'react-native';
+import { Text, Button } from 'react-native-paper';
 
 const InmobiliariaProfile = () => {
-  return <Text>InmobiliariaProfile</Text>;
+  const fetchedInmobiliaria = {
+    name: 'Inmobiliaria 1',
+    email: 'some@gmail.com0',
+    phone: '1234567890',
+    address: 'Some address 123',
+    cuit: '1234567890',
+  }
+  return <View style={styles.container}>
+    <Text>InmobiliariaProfile</Text>
+    <Text>{fetchedInmobiliaria.name}</Text>
+    <Text>{fetchedInmobiliaria.email}</Text>
+    <Text>{fetchedInmobiliaria.phone}</Text>
+    <Text>{fetchedInmobiliaria.address}</Text>
+    <Text>{fetchedInmobiliaria.cuit}</Text>
+
+
+    <Button mode='outlined'>
+      <Text>Cerrar sesion</Text>
+    </Button>
+
+    <Button mode='contained'>
+      <Text>Eliminar cuenta</Text>
+    </Button>
+  </View>
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    display: 'flex',
+    direction: 'column',
+  },
+});
 
 export default InmobiliariaProfile;
